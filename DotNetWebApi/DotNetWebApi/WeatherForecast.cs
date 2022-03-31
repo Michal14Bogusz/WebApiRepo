52b2bd33e44137b277b7fd858138595a06714156
Michal Bogusz <Michal.Bogusz@GAINCapital.com>
2022-04-01 01:00:56 +0200

    Creation of the web api project

---
+++ DotNetWebApi/DotNetWebApi/WeatherForecast.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DotNetWebApi
+{
+    public class ExemplaryClass
+    {
+        public DateTime Date { get; set; }
+
+        public int ExemplaryField { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WebApiRepo
 This is the exemplary repo dedicated to Api project.
+
+1. Basic scaffold of the web api - creation of the project.
